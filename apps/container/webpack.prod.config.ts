1e64c8f99401f7168ff111e53967dfe5f3316a19
Michael Mountain <michael.mountain@ayo4u.com>
2024-06-06 12:43:08 +0200

    release/0.1 - set up firebase and release all apps v0.1

--- apps/container/webpack.prod.config.ts
+++ apps/container/webpack.prod.config.ts
@@ -6,11 +6,12 @@ export default withModuleFederation({
   /*
    * Remote overrides for production.
    * Each entry is a pair of a unique name and the URL where it is deployed.
-   *
-   * e.g.
-   * remotes: [
-   *   ['app1', 'https://app1.example.com'],
-   *   ['app2', 'https://app2.example.com'],
-   * ]
    */
+  remotes: [
+    ['resume', 'https://phoenix-resume.firebaseapp.com'],
+    ['budgets', 'https://phoenix-budgets.firebaseapp.com'],
+    ['code-master', 'https://phoenix-code-master.firebaseapp.com'],
+    ['gallery', 'https://phoenix-gallery.firebaseapp.com'],
+    ['movies', 'https://phoenix-movie-app.firebaseapp.com'],
+  ]
 });
