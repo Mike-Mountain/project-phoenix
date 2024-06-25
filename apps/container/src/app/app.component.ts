1c257395ee2a526c2b460c02e6008a61034b7ef6
Michael Mountain <michael.mountain@ayo4u.com>
2024-06-25 14:21:53 +0200

    feature/auth - Update to latest nx + add angular/fire

--- apps/container/src/app/app.component.ts
+++ apps/container/src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { LayoutComponent } from '@project-phoenix/container/container-ui';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @Component({
   standalone: true,
