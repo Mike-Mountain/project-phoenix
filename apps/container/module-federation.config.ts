3bea71cb69576d0b31ef89e6e80c9f63b28fd866
Michael Mountain <michael.mountain@ayo4u.com>
2024-08-20 18:39:49 +0200

    feature/gitlab-ci - Update module federation config

--- apps/container/module-federation.config.ts
+++ apps/container/module-federation.config.ts
@@ -14,14 +14,14 @@ const config: ModuleFederationConfig = {
    * declare module 'my-external-remote';
    *
    */
-  // remotes: ['code-master', 'resume', 'movies', 'budgets', 'gallery', 'groups'],
+  remotes: ['code-master', 'resume', 'movies', 'budgets', 'gallery', 'groups'],
   // remotes: [],
   // remotes: ['resume'],
   // remotes: ['code-master'],
   // remotes: ['movies'],
   // remotes: ['budgets'],
   // remotes: ['gallery'],
-  remotes: ['groups'],
+  // remotes: ['groups'],
 };
 
 export default config;
--- apps/container/webpack.prod.config.ts
+++ apps/container/webpack.prod.config.ts
@@ -8,10 +8,11 @@ export default withModuleFederation({
    * Each entry is a pair of a unique name and the URL where it is deployed.
    */
   remotes: [
-    ['resume', 'https://phoenix-resume.firebaseapp.com'],
-    ['budgets', 'https://phoenix-budgets.firebaseapp.com'],
-    ['code-master', 'https://phoenix-code-master.firebaseapp.com'],
-    ['gallery', 'https://phoenix-gallery.firebaseapp.com'],
-    ['movies', 'https://phoenix-movie-app.firebaseapp.com'],
+    ['resume', 'https://resume.honeystonemountain.com'],
+    ['budgets', 'https://budgets.honeystonemountain.com'],
+    ['code-master', 'https://code-master.honeystonemountain.com'],
+    ['gallery', 'https://gallery.honeystonemountain.com'],
+    ['movies', 'https://movies.honeystonemountain.com'],
+    ['groups', 'https://groups.honeystonemountain.com'],
   ]
 });
