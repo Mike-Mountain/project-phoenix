6a72822147051321010733b82d4cb8db5a2d0f5c
Michael Mountain <michael.mountain@ayo4u.com>
2024-08-23 08:51:37 +0200

    feature/gitlab-ci - update webpack prod config

--- apps/container/webpack.prod.config.ts
+++ apps/container/webpack.prod.config.ts
@@ -8,11 +8,11 @@ export default withModuleFederation({
    * Each entry is a pair of a unique name and the URL where it is deployed.
    */
   remotes: [
-    ['resume', 'http://172.18.0.7:80'],
-    ['budgets', 'http://172.18.0.3:80'],
-    ['code-master', 'http://172.18.0.9:80'],
-    ['gallery', 'http://172.18.0.8:80'],
-    ['movies', 'http://172.18.0.6:80'],
-    ['groups', 'http://172.18.0.5:80'],
+    ['resume', 'https://resume.honeystonemountain.com'],
+    ['budgets', 'https://budgets.honeystonemountain.com'],
+    ['code-master', 'https://code-master.honeystonemountain.com'],
+    ['gallery', 'https://gallery.honeystonemountain.com'],
+    ['movies', 'https://movies.honeystonemountain.com'],
+    ['groups', 'https://groups.honeystonemountain.com'],
   ]
 });
