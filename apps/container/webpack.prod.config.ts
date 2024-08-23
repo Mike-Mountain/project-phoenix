a7c5f741b6d7b3c1f80106f5305ed00ba8bf0508
Michael Mountain <michael.mountain@ayo4u.com>
2024-08-23 10:40:55 +0200

    feature/gitlab-ci - update container webpack config

--- apps/container/webpack.prod.config.ts
+++ apps/container/webpack.prod.config.ts
@@ -8,11 +8,11 @@ export default withModuleFederation({
    * Each entry is a pair of a unique name and the URL where it is deployed.
    */
   remotes: [
-    ['resume', 'http://resume:80'],
-    ['budgets', 'http://budgets:80'],
-    ['code-master', 'http://code-master:80'],
-    ['gallery', 'http://gallery:80'],
-    ['movies', 'http://movies:80'],
-    ['groups', 'http://groups:80'],
+    ['resume', 'resume'],
+    ['budgets', 'budgets'],
+    ['code-master', 'code-master'],
+    ['gallery', 'gallery'],
+    ['movies', 'movies'],
+    ['groups', 'groups'],
   ]
 });
